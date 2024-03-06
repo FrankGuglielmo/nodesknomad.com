21b66657c9ca3c9cf789172589b67ac714ea3a13
Frank Guglielmo <frankieg1610@gmail.com>
2024-03-06 09:49:20 -0800

    Added Tailwind CSS and created initial structure

--- src/App.js
+++ src/App.js
@@ -16,9 +16,34 @@ const App = () => {
   }, []);
 
   return (
-    <div style={{ display: 'flex' }}>
-      <MapComponent coffeeShops={coffeeShops} />
-      {/* Add more components here such as LocationDetails */}
+    <div className="flex flex-col h-screen">
+      {/* Nav bar */}
+      <div className="bg-purple-600 shadow-lg p-4 text-white text-center">
+        NoDeskNomad.com
+      </div>
+      
+      {/* Content */}
+      <div className="flex-grow flex p-4 space-x-4">
+        {/* Map component div */}
+        <div className="flex-1 bg-white shadow-lg p-4">
+          {/* Map content here */}
+          <MapComponent coffeeShops={coffeeShops} />
+        </div>
+        
+        {/* Coffee shops list div */}
+        <div className="flex-1 bg-white shadow-lg p-4">
+          {/* Coffee shops list content here */}
+          <p>Coffee Shops List</p>
+        </div>
+      </div>
+
+      {/* Sign in prompt */}
+      <div className="w-full bg-white shadow-lg p-4 text-center">
+        <p>Sign in or Sign up to gain access to more locations!</p>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Sign in with Google
+        </button>
+      </div>
     </div>
   );
 };
